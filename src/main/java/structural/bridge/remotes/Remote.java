43f33511633534cab8d502caeb473bd1fe1723d2
Artem Kuznetsov <at0m616@yandex.ru>
2021-12-21 14:10:05 +0500

    Add structural example

---
+++ src/main/java/structural/bridge/remotes/Remote.java
@@ -0,0 +1,14 @@
+package structural.bridge.remotes;
+
+//Общий интерфейс всех пультов ДУ
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
